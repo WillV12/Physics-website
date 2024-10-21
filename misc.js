c0296355e6c38aed3338a03acd8d3d330b0d0224
WillV12 <120140873+WillV12@users.noreply.github.com>
2024-10-21 09:58:28 -0400

    added random color function

--- misc.js
+++ misc.js
@@ -5,7 +5,7 @@
 
 const reset = document.getElementById('reset')
 const colorButton = document.getElementById('colorButton')
-const dataview = document.getElementById('data')
+const random = document.getElementById("randomize")
 
 reset.addEventListener('click', (event) =>{
 
@@ -23,5 +23,18 @@ colorButton.addEventListener('click', (event) =>{
 })
 
 
+const hexList = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+
+random.addEventListener('click', (event) =>{
+
+    let colors = "#"
+
+    for (let i = 0; i < 6; i++){
+        index = parseInt(Math.random() * hexList.length)
+        colors += hexList[index]}
+    
+    color(colors)
+    alert("Color has been set to " + colors )
+})
 
     
